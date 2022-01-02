e1161ad6f54310f1af6d53f9efa6686dcef0f7fd
maria alkiviadous <M.Alkiviadous2@newcastle.ac.uk>
2022-01-02 10:14:55 +0000

    Update app.py
    Updated the file, so that it contains lines of code that allow me to view the home and about pages.

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+# IMPORTS
+import logging
+import socket
+from functools import wraps
+from flask import Flask, render_template, request
+from flask_login import LoginManager, current_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    # BLUEPRINTS
+    # import blueprints
