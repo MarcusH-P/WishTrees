b4547f3047a82c745665b4b050793e80a473e2bb
maria alkiviadous <M.Alkiviadous2@newcastle.ac.uk>
2022-01-10 09:08:38 +0000

    Update app.py
    Changes were made so that the newly created and updated pages could be displayed

--- app.py
+++ app.py
@@ -20,6 +20,40 @@ def about():
     return render_template('about.html')
 
 
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+
+@app.route("/games")
+def games():
+    return render_template('games.html')
+
+
+@app.route("/charities")
+def charities():
+    return render_template('charities.html')
+
+
+@app.route("/donate")
+def donate():
+    return render_template('donate.html')
+
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
