7b0cd77ac278d0110b6bfef9369f56683e8c6228
c0051643 <NorthernSenseHere19072002>
2022-01-17 17:41:06 +0000

    added QR code generator for 2fa

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 import datetime
+import base64
+import os
+import onetimepass
 from app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
-class User(db.Model, UserMixin):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-
 
     # User authentication information.
     email = db.Column(db.String(100), nullable=False, unique=True)
@@ -25,17 +28,27 @@ class User(db.Model, UserMixin):
     phone = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False, default='user')
 
-    def __init__(self, email, firstname, lastname, phone, password, pin_key, role):
+    def __init__(self, email, firstname, lastname, phone, password, role):
         self.email = email
         self.firstname = firstname
         self.lastname = lastname
         self.phone = phone
-        self.password = generate_password_hash(password) # IS THIS HASH GOOD? IDK PROBABLY
-        self.pin_key = pin_key
+        self.password = generate_password_hash(password)  # IS THIS HASH GOOD? IDK PROBABLY
         self.role = role
         self.registered_on = datetime.now()
         self.last_logged_in = None
         self.current_logged_in = None
+        if self.otp_secret is None:
+            self.otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')  # Creates One Time Password secret
+        self.otp_setup = None
+
+    def get_uri(self):  # Generates Uri for 2FA QR Code
+        return 'otpauth://totp/WishTrees:{0}?secret={1}&issuer=WishTrees' \
+            .format(self.email, self.otp_secret)
+
+    def verify_otp(self, token):
+        return onetimepass.valid_totp(self.otp_secret, token)
+
 
 def init_db():
     db.drop_all()
@@ -45,6 +58,7 @@ def init_db():
                  pin_key='BFB5S34STBLZCOB22K6PPYDCMZMH46OJ',
                  firstname='Alice',
                  lastname='Jones',
-                 role='admin')
+                 role='admin',
+                 phone='')
     db.session.add(admin)
     db.session.commit()
