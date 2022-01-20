df2c53b113a096625f21df8b113981f64e10ebff
marcushunltypeck <m.huntly-peck1@newcastle.ac.uk>
2022-01-20 03:52:32 +0000

    Removed donate function to render the donate.html template in app.py

--- app.py
+++ app.py
@@ -93,11 +93,6 @@ def charities():
     return render_template('charities.html')
 
 
-@app.route("/donate")
-def donate():
-    return render_template('donate.html')
-
-
 @app.route("/admin")
 def admin():
     return render_template('admin.html')
