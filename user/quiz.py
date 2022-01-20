861bba2f9add464ff26b307c5f30459778f27137
Kamran Aslam <k.aslam1@ncl.ac.uk>
2022-01-20 22:34:20 +0000

    Tidied up quiz1.html

--- user/quiz.py
+++ user/quiz.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, Blueprint, request
-import random
-# import copy
+import random, copy
 
 quiz_blueprint = Blueprint('quiz', __name__, template_folder='templates')
 
