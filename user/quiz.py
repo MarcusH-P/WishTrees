c5d329721ec7ab448bf00e5d51539b5884381d2b
Kamran Aslam <k.aslam1@ncl.ac.uk>
2022-01-21 01:57:37 +0000

    Created second and third quiz to quiz.py

--- user/quiz.py
+++ user/quiz.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Blueprint, request
+from flask import Flask, render_template, Blueprint, request, session
 from flask_login import current_user
 from app import db_add_commit
 import random
@@ -6,6 +6,7 @@ import copy
 
 quiz_blueprint = Blueprint('quiz', __name__, template_folder='templates')
 
+# quiz 1 questions and answers
 wishtrees_questions = {
     # Format is 'question':[options]
     'How much money has WishTrees raised': ['£10 million', '£1 billion', '£500,000', '£250,000'],
@@ -15,7 +16,33 @@ wishtrees_questions = {
     'What can you do to help': ['All of the above', 'Donate to charities', 'Buy our merch', 'Complete our quizzes']
 }
 
-questions = copy.deepcopy(wishtrees_questions)
+# quiz 2 questions and answers
+LifeOnLand_questions = {
+    # Format is 'question':[options]
+    'What is the aim of the Life on Land sustainable goal': ['All of the above',
+                                                             'Protect, restore and promote sustainable use of terrestrial systems',
+                                                             'Sustainably manage forests and combat desertification',
+                                                             'Halt and reverse land degradation'],
+    'How many targets does the UN aim to achieve with this goal': ['12', '4', '9', '20'],
+    'By what year does the UN want to end desertification and restore degraded land': ['2030', '2040', '2050', '2025'],
+    'How do we protect endangered species': ['All of the above', 'Prevent poaching', 'End trafficking of animals',
+                                             'Address the supply and demand of illegal wildlife products'],
+    'How is the UN making this goal attractive to key players': ['Incentevise sustainable restructure',
+                                                                 'Increasing taxes',
+                                                                 'Encouraging unethical consumption',
+                                                                 'Raising the prices of raw materials']
+}
+
+# quiz 3 questions and answers
+PowerPlanters_questions = {
+    # Format is 'question':[options]
+    'How many acres of forest are destroyed each day': ['80,000', '400', '5,000', '41,000'],
+    'How many trees would an average US citizen have to plant to be carbon neutral': ['620', '28', '1300', '370'],
+    'How many species of plants, insects and animals are lost each day due to deforestation': ['137', 'None', '32',
+                                                                                               '124'],
+    'How many trees have been lost since the start of civilisation': ['34%', '8%', '50%', '18%'],
+    'Which causes more carbon emissions': ['Deforestation', 'Vehicles', 'Factories', 'household energy']
+}
 
 
 # This function is for shuffling the dictionary elements.
@@ -30,22 +57,49 @@ def shuffle(q):
     return selected_keys
 
 
-@quiz_blueprint.route('/quiz')
+@quiz_blueprint.route('/quiz1')
 def quiz1():
+    questions = copy.deepcopy(wishtrees_questions)
     questions_shuffled = shuffle(questions)
     for i in questions.keys():
         random.shuffle(questions[i])
+    session['questions'] = questions
+    session['answers'] = wishtrees_questions
+    return render_template('quiz1.html', q=questions_shuffled, o=questions)
+
+
+@quiz_blueprint.route('/quiz2')
+def quiz2():
+    questions = copy.deepcopy(LifeOnLand_questions)
+    questions_shuffled = shuffle(questions)
+    for i in questions.keys():
+        random.shuffle(questions[i])
+    session['questions'] = questions
+    session['answers'] = LifeOnLand_questions
+    return render_template('quiz1.html', q=questions_shuffled, o=questions)
+
+
+@quiz_blueprint.route('/quiz3')
+def quiz3():
+    questions = copy.deepcopy(PowerPlanters_questions)
+    questions_shuffled = shuffle(questions)
+    for i in questions.keys():
+        random.shuffle(questions[i])
+    session['questions'] = questions
+    session['answers'] = PowerPlanters_questions
     return render_template('quiz1.html', q=questions_shuffled, o=questions)
 
 
 @quiz_blueprint.route('/quiz', methods=['POST'])
 def quiz_answers():
     correct = 0
+    questions = session['questions']
+    answers = session['answers']
     for i in questions.keys():
         answered = request.form[i]
-        if wishtrees_questions[i][0] == answered:
+        if answers[i][0] == answered:
             correct = correct + 1
     the_user = current_user
-    the_user.points = current_user.points + (correct*100)
+    the_user.points = current_user.points + (correct * 100)
     db_add_commit(the_user)
     return '<h1>Correct Answers: <u>' + str(correct) + '</u></h1>'
